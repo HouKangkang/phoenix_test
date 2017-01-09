cdbb27487fa4e9562eb16bd61c5aeace44412830
houkangkang <kangkangh@everstring.com>
2017-01-09 12:47:58 +0800

    fix file upload

--- web/controllers/FileController.ex
+++ web/controllers/FileController.ex
@@ -14,10 +14,19 @@ defmodule HelloPhoenix.FileController do
     file_name = random_file_name <> extension
 
     File.cp(file.path, "/tmp/" <> file_name)
+#    IO.inspect(get_orgin(conn.req_headers))
 #    api_suc(conn, 201, "http://localhost:4000/uploads/" <> file_name)
-#    api_suc(conn, 201, Enum.join(["http://", conn.host, ":", conn.port, "/uploads/", file_name], ""))
-    api_suc(conn, 201, Enum.join([Router.Helpers.url(conn), "/uploads/", file_name], ""))
+#    api_suc(conn, 201, Enum.join([conn.scheme, "://", conn.host, ":", conn.port, "/uploads/", file_name], ""))
+    api_suc(conn, 201, Enum.join([get_orgin(conn.req_headers), "/uploads/", file_name], ""))
 
+#    IO.puts("url from connection: #{Router.Helpers.url(conn)}")
+#    api_suc(conn, 201, Enum.join([Router.Helpers.url(conn), "/uploads/", file_name], ""))
+
+  end
+
+  defp get_orgin(headers) do
+    item = Enum.find(headers, &(elem(&1,0) == "origin"))
+    elem(item, 1)
   end
 
 end
